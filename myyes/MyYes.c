56b3d164128047a2dd1e056d19d87a28a5016a2c
Ne-GH <printfne@gmail.com>
2022-01-03 20:50:04 +0800

    yes

--- myyes/MyYes.c
+++ myyes/MyYes.c
@@ -2,33 +2,34 @@
 #include <string.h>
 #include <unistd.h>
 
-#define BUFFERSIZE 1024 * 8
+#define BUFFERSIZE (1024*8)
 
 char buf[BUFFERSIZE];
+void GetStr(int argc,char **argv){
+    int pos = argc - 1;
+    int len = strlen(argv[pos]);
 
-int main(){
-    buf[0] = 'y';
-    buf[1] = '\n';
-    memcpy(buf+2, buf, 2);
-    memcpy(buf+4, buf, 4);
-    memcpy(buf+8, buf, 8);
-    memcpy(buf+16, buf, 16);
-    memcpy(buf+32, buf, 32);
-    memcpy(buf+64, buf, 64);
-    memcpy(buf+128, buf, 128);
-    memcpy(buf+256, buf, 256);
-    memcpy(buf+512, buf, 512);
-    memcpy(buf+1024, buf, 1024);
-    memcpy(buf+(1024*2), buf, (1024*2));
-    memcpy(buf+(1024*3), buf, (1024*3));
-    memcpy(buf+(1024*4), buf, (1024*4));
+    // 在 buf 中存放第一个字符串
+    memcpy(buf, argv[pos], len);
+    buf[len++] = '\n';
 
+    int use = 0;
+    for(int i = len;i < BUFFERSIZE;i *= 2){
+        memcpy(buf+i,buf, i);
+    }
+
+    for(int i = 0;i < BUFFERSIZE ; ++i){
+        printf("%d ",buf[i]);
+    }
+
+}
+
+int main(int argc,char **argv){
+    GetStr(argc, argv);
 
 Go:
         write(1,buf,BUFFERSIZE);
         goto Go;
-
-    
 
 
     return 0;
